78fa5a4694ae62eca775425cf01bb7e529b1da09
Vass <vass@debian>
2015-08-23 19:00:58 +0300

    My "cache matrix"

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 1. The makeCacheMatrix function creates a special "matrix" object that can cache its inverse, 
+## that is really a list containing a function to:
+## 1) set the value of the matrix;
+## 2) get the value of the matrix;
+## 3) set the inverse of a matrix;
+## 4) get the inverse of a matrix.
+## 2. The cacheSolve function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix. If the inverse has already been calculated (and the matrix has not 
+## changed), then the cacheSolve retrieves the inverse from the cache.
 
-## Write a short comment describing this function
 
+## Creating a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(data) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x 
+  setinverse <- function(inverse) inv <<- inverse 
+  getinverse <- function() inv 
+  list(set = set, get = get, 
+       setinverse = setinverse, 
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## Computing the inverse for special "matrix" or reterning it from cache. 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
+
+##the following lines are meant for testing
+#set.seed(1) 
+#my_matr <- matrix(rnorm(1000000), nrow = 1000, ncol = 1000)
+#qq <- makeCacheMatrix(my_matr)
+#slv <- solve(my_matr)
+#cslv <- cacheSolve(qq)
+#print(c(max(slv - cslv), min(slv - cslv)))
+#cslv1 <- cacheSolve(qq)
+#print(c(max(slv - cslv1), min(slv - cslv1)))
